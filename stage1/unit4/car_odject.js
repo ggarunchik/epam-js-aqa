19dec6d55271c52b05425c1f479b694f8f8ba322
Gleb Garunchik <garunchikgleb@gmail.com>
2021-07-19 22:15:16 +0300

    Added additional js file with class Car

---
+++ stage1/unit4/car_odject.js
@@ -0,0 +1,18 @@
+// В целях тренировки пробую иной способ создания объекта из юнита4
+class Car {
+    constructor(model, color, carPower){
+        this.model = model,
+        this.color = color,
+        this.carPower = carPower
+    }
+
+    power() {
+        return this.carPower
+    } 
+
+}
+
+let newCar = new Car('volvo', 'black', 245)
+newCar.color = 'Green'
+console.log(newCar)
+console.log(newCar.power())
