ddbc36ccb5f7ef4f0f59597b66e50e2459fa9509
Gleb Garunchik <garunchikgleb@gmail.com>
2021-07-21 21:57:59 +0300

    Added home work for unit6

---
+++ stage1/unit6/classes_and_inheritance.js
@@ -0,0 +1,37 @@
+// Создать 2 объекта: animal и cat, объект animal добавить свойство move, объект cat должен наследовать свойство move
+class Animal {
+    constructor(name) {
+        this.speed = 0
+        this.name = name
+    }
+
+    move(speed) {
+        this.speed = speed
+        console.log(`${this.name} is moving with speed ${this.speed}`)
+    }
+}
+
+class Cat extends Animal {
+    constructor (name, color) {
+        super(name)
+        this.color
+    }
+}
+
+let myCat = new Cat('Jojo', 'Black')
+myCat.move(34)
+
+// Тут решил попробовать прототипное наследование
+let animalProto = {
+    speed: 0,
+    moveProto(newSpeed) {
+        this.speed = newSpeed
+        console.log(`moving with speed ${this.speed}`)
+    }
+  }
+  
+  let catProto = {
+    __proto__: animalProto
+  };
+
+  catProto.moveProto(43)
