ccc66963291567ae8dc9e1c24f56462a9c0a6cdc
Gleb Garunchik <garunchikgleb@gmail.com>
2021-08-13 11:01:53 +0300

    split long console log into 'short' lines

--- stage1/unit3/statements_data_types.js
+++ stage1/unit3/statements_data_types.js
@@ -7,41 +7,41 @@ const sumStringAndBoolean = stringValue + booleanValue;
 const sumStringAndNumber = stringValue + numberValue;
 const sumNumberAndBoolean = numberValue + booleanValue;
 
-console.log('string('+ stringValue + ') + boolean:('+ booleanValue + ') ' + sumStringAndBoolean + '\n' +
-            'string('+ stringValue + ') + number:('+ numberValue + ') ' + sumStringAndNumber + '\n' +
-            'number('+ numberValue + ') + boolean:('+ booleanValue + ') ' + sumNumberAndBoolean);
+console.log('string('+ stringValue + ') + boolean:('+ booleanValue + ') ' + sumStringAndBoolean) 
+console.log('string('+ stringValue + ') + number:('+ numberValue + ') ' + sumStringAndNumber)
+console.log('number('+ numberValue + ') + boolean:('+ booleanValue + ') ' + sumNumberAndBoolean)
 
 console.log('\nВыполнить умножение различных типов(string * boolean, string * number, number * boolean)');
 const mulStringAndBoolean = stringValue * booleanValue;
 const mulStringAndNumber = stringValue * numberValue;
 const mulNumberAndBoolean = numberValue * booleanValue;
 
-console.log('string('+ stringValue + ') * boolean:('+ booleanValue + ') ' + mulStringAndBoolean + '\n' +
-            'string('+ stringValue + ') * number:('+ numberValue + ') ' + mulStringAndNumber + '\n' +
-            'number('+ numberValue + ') * boolean:('+ booleanValue + ') ' + mulNumberAndBoolean);
+console.log('string('+ stringValue + ') * boolean:('+ booleanValue + ') ' + mulStringAndBoolean)
+console.log('string('+ stringValue + ') * number:('+ numberValue + ') ' + mulStringAndNumber)
+console.log('number('+ numberValue + ') * boolean:('+ booleanValue + ') ' + mulNumberAndBoolean)
 
 console.log('\nВыполнить деление различных типов(string/boolean, string/number, number/Boolean)')
 const divStringAndBoolean = stringValue / booleanValue;
 const divStringAndNumber = stringValue / numberValue;
 const divNumberAndBoolean = numberValue / booleanValue;
 
-console.log('string('+ stringValue + ') / boolean:('+ booleanValue + ') ' + divStringAndBoolean + '\n' +
-            'string('+ stringValue + ') / number:('+ numberValue + ') ' + divStringAndNumber + '\n' +
-            'number('+ numberValue + ') / boolean:('+ booleanValue + ') ' + divNumberAndBoolean)
+console.log('string('+ stringValue + ') / boolean:('+ booleanValue + ') ' + divStringAndBoolean)
+console.log('string('+ stringValue + ') / number:('+ numberValue + ') ' + divStringAndNumber)
+console.log('number('+ numberValue + ') / boolean:('+ booleanValue + ') ' + divNumberAndBoolean)
 
 console.log('\nВыполнить явное преобразование(number, string, boolean)');
 const stringToNumber = Number(stringValue);
 const numberToString = String(numberValue);
 const numberToBoolean = Boolean(numberValue);
 
-console.log('Start is: ' + stringValue + '\n' + 
-            'Old typeof is: ' + typeof(stringValue) + '\n' + 
-            'New typeof is: ' + typeof(stringToNumber)+ '\n');
+console.log('Start is: ' + stringValue) 
+console.log('Old typeof is: ' + typeof(stringValue))
+console.log('New typeof is: ' + typeof(stringToNumber))
 
-console.log('Start is: ' + numberValue + '\n' + 
-            'Old typeof is: ' + typeof(numberValue) + '\n' + 
-            'New typeof is: ' + typeof(numberToString)+ '\n');
+console.log('Start is: ' + numberValue)
+console.log('Old typeof is: ' + typeof(numberValue))
+console.log('New typeof is: ' + typeof(numberToString))
 
-console.log('Start is: ' + numberValue + '\n' + 
-            'Old typeof is: ' + typeof(numberValue) + '\n' + 
-            'New typeof is: ' + typeof(numberToBoolean)+ '\n');
+console.log('Start is: ' + numberValue)
+console.log('Old typeof is: ' + typeof(numberValue))
+console.log('New typeof is: ' + typeof(numberToBoolean))
